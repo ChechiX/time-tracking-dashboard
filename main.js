add440cb0d6873ac6a67faba1bbd8dfeeb8590ca
Julio Anccasi <julio.anccasi.dev@gmail.com>
2026-02-11 22:58:50 -0500

    final commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const activityCards = document.querySelectorAll('.section');
+const timeFrameButtons = document.querySelectorAll(
+  '.time-frame-selector__button',
+);
+
+// const timeFrameButtons = [dailyBtn, weeklyBtn, monthlyBtn];s
+
+let activitiesData = [];
+
+const labels = {
+  daily: 'Yesterday',
+  weekly: 'Last Week',
+  monthly: 'Last Month',
+};
+
+const updateCards = (timeframe, data) => {
+  data.forEach((activity) => {
+    const matchedCard = [...activityCards].find((card) => {
+      return card.dataset.activity === activity.title;
+    });
+
+    const hoursElement = matchedCard.querySelector('h3');
+    const previousHoursElement = matchedCard.querySelector('p');
+
+    hoursElement.textContent = `${activity.timeframes[timeframe].current}hrs`;
+    previousHoursElement.textContent = `${labels[timeframe]} - ${activity.timeframes[timeframe].previous}hrs`;
+  });
+};
+
+timeFrameButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    timeFrameButtons.forEach((btn) => {
+      btn.classList.remove('time-frame-selector__button--active');
+    });
+
+    button.classList.add('time-frame-selector__button--active');
+
+    const selectedTimeframe = button.textContent.trim().toLowerCase();
+
+    console.log(selectedTimeframe);
+    updateCards(selectedTimeframe, activitiesData);
+  });
+});
+
+fetch('./data.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    activitiesData = data;
+
+    updateCards('weekly', data);
+  });
